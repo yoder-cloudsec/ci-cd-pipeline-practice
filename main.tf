8933281151d14dc7b348c1349fbee0e83bac3492
Yoder Cloud-Sec <bmyoder1988@gmail.com>
2026-09-09 11:03:34 +0200

    Migrate to remote state

--- main.tf
+++ main.tf
@@ -5,12 +5,20 @@ terraform {
         version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "yoder-terraform-state-bucket"
+    key = "cicd-pipeline-practice/terraform.tfstate"
+    region = "eu-south-2"
+    dynamodb_table = "terraform-locks"
+    encrypt = true
+  }
 }
 
 provider "aws" {
   region = "eu-south-2"
 }
-# demo bucket for CI/CD pipeline practice
+
 resource "aws_s3_bucket" "demo" {
   bucket = "yoder-cicd-pipeline-demo"
 
